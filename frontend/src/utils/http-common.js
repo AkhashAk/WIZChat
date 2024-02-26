5120cc8eda1d0b4ef0f18bb34c891318a964dfda
AkhashAk <giriakhash@gmail.com>
2024-02-26 15:04:33 +0530

    update

--- frontend/src/utils/http-common.js
+++ frontend/src/utils/http-common.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const axiosAPI = axios.create({
-  // baseURL: "https://ylqz0rb9xf.execute-api.us-east-1.amazonaws.com/user",
-  // baseURL: "https://64db5b59593f57e435b0d352.mockapi.io/users",
   baseURL: import.meta.env.VITE_BACKEND_API_URL,
   headers: {
     "Content-type": "application/json"
